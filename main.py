1f4bc1f55384f544b355eac85bbf0cd3a70f332e
rosilioroman <rosilioroman@gmail.com>
2024-04-12 18:37:21 -0600

    Add main.py skeleton including command line argument parsing.

--- main.py
+++ main.py
@@ -4,8 +4,56 @@ Author: Rosilio Roman
 Main script that controls program logic
 '''
 
-def main():
-    print("Main script running...")
+import argparse
+from src.data_loader import load_data
+# from src.analysis import perform_analysis
+# from src.simulation import run_simulation
+# from src.visualization import visualize_results
+
+def main(data_subdir):
+
+    '''
+    Loading Data
+    '''
+    # Build the full path to the data directory based on command line argument
+    data_path = f'data/{data_subdir}'
+
+    # Load data from the specified source
+    print(f"Loading data from {data_path}...")
+    data = load_data(data_path)
+
+    '''
+    Processing Data
+    '''
+    # # Perform data preprocessing and analysis
+    # print("Analyzing data...")
+    # processed_data = perform_analysis(data)
+
+    '''
+    Simulating
+    '''
+    # # Run simulation with the processed data
+    # print("Running simulations...")
+    # simulation_results = run_simulation(processed_data)
+
+    '''
+    Visualize Data
+    '''
+    # # Visualize the results of the simulation
+    # print("Visualizing results...")
+    # visualize_results(simulation_results)
+
+    if data == True:
+        print("Process completed successfully.")
+    else:
+        print("Process failed.")
+
+def parse_arguments():
+    parser = argparse.ArgumentParser(description='Run the CST-G-Value-Plot simulation and analysis.')
+    parser.add_argument('data_subdir', type=str, help='Name of subdirectory under data/ containing the input files.')
+    args = parser.parse_args()
+    return args
 
 if __name__ == "__main__":
-    main()
+    args = parse_arguments()
+    main(args.data_subdir)
--- src/data_loader.py
+++ src/data_loader.py
@@ -3,3 +3,21 @@ Author: Rosilio Roman
 
 Contains functions to read and preprocess data.
 '''
+
+import os
+
+def load_data(data_path):
+
+    current_directory = os.getcwd()
+    full_path = os.path.join(current_directory, data_path)
+
+    # Check that the given data_path is valid
+    print("\tChecking data subdirectory exists...")
+
+        # CHANGE THIS TO MORE ROBUST ERROR HANDLING
+    if os.path.exists(full_path):
+        print("\tSuccess!")
+        return True
+    else:
+        print("\tDid not find the data subdirectory.")
+        return False
